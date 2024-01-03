c06a7c2b403832bda599a4a3741aa3d1628afd83
Adnan Sarguroh <146064088+Adnan09123@users.noreply.github.com>
2024-01-03 18:43:55 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react';
 import Header from './Header';
 import RestaurantsDisplay from './Restaurants';
 
-const url = "http://13.235.114.118:8443/restaurant/";
+const url = "http://18.246.219.227:8443/restaurant/";
 
 class Home extends Component{
     constructor(){
